08e512d86abbdb1be0bea06e5cc4fe24f6d95017
Maxime Thibaud <maximethibaudwcs@gmail.com>
2020-01-29 18:03:10 +0100

    new routes for artists. Get one artist and get all artists

--- back/index.js
+++ back/index.js
@@ -15,6 +15,9 @@ app.use('/api/categories', categories);
 const performances = require('./routes/performances');
 app.use('/api/performances', performances);
 
+const artists = require('./routes/artists');
+app.use('/api/artists', artists);
+
 app.listen(port, (err) => {
   if (err) {
     throw new Error('Something bad happened...');
