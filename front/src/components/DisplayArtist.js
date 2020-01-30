664e9a8010e34b46d8f60c1457dd929042a21378
Maxime Thibaud <maximethibaudwcs@gmail.com>
2020-01-30 08:28:47 +0100

    transform DisplayArtist in class

--- front/src/components/DisplayArtist.js
+++ front/src/components/DisplayArtist.js
@@ -2,7 +2,15 @@ import React from 'react';
 // import './Display.css';
 // import './Nicebutton.css'
 
-function DisplayArtist(props){
+class DisplayArtist extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      places : [],
+      currentPlace: 0,
+    }
+    // this.nextPlace = this.nextPlace.bind(this)
+  }
 
   // nextPlace() {
   //   this.setState(prevState => {
@@ -25,19 +33,21 @@ function DisplayArtist(props){
   //   alt: 'profil du vacancier'
   // }
 
-  return(
-  <div id="pagePlace">
-    {/* <div id="ProfilPlace">
-    <img className="photoProfil" src={imgValues.src} alt={imgValues.alt} ></img>
-    </div>
-    <h2 className="titreProfil">{props.artist.firstname} {props.artist.lastname}</h2>
-    <div className="desc">
-      <p>{props.artist.description_artist}</p>
-    </div>
-    <button className='buttonNextVacationer'  type="button" onClick={props.nextPlace}>Suivant</button> */}
-    <p>hello</p>
-  </div>
-  )
+  render() {
+    return (
+      <div id="pagePlace">
+        {/* <div id="ProfilPlace">
+        <img className="photoProfil" src={imgValues.src} alt={imgValues.alt} ></img>
+        </div>
+        <h2 className="titreProfil">{props.artist.firstname} {props.artist.lastname}</h2>
+        <div className="desc">
+          <p>{props.artist.description_artist}</p>
+        </div>
+        <button className='buttonNextVacationer'  type="button" onClick={props.nextPlace}>Suivant</button> */}
+        <p>hello</p>
+      </div>
+    )
+  }
 }
 
 export default DisplayArtist;
